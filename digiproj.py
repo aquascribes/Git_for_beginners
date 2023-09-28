f63fd8ce72debc521f0bfee391903066d9d3aa74
aquascribes <aqsamushtaq@gmail.com>
2023-09-28 23:56:32 +0200

    added window title & changed color

--- digiproj.py
+++ digiproj.py
@@ -1,10 +1,10 @@
 from tkinter import Tk, Label
 
 window=Tk()
-window.title("digital clock")
+window.title("Window")
 window.geometry("600x300")
-window.configure(bg="red")
+window.configure(bg="blue")
 
-label=Label(window,text="welcome",font=("Arial Black",78,"bold"),bg="red")
+label=Label(window,text="welcome",font=("Arial Black",78,"bold"),bg="blue")
 label.pack(pady=100)
 window.mainloop()
