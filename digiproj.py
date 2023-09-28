8ff0005a3d36dcb943dfc954ffba238309bed136
aquascribes <aqsamushtaq@gmail.com>
2023-09-28 16:44:53 +0200

    made red background

--- digiproj.py
+++ digiproj.py
@@ -3,8 +3,8 @@ from tkinter import Tk, Label
 window=Tk()
 window.title("digital clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="red")
 
-label=Label(window,text="welcome",font=("Arial Black",78,"bold"),bg="steelblue")
+label=Label(window,text="welcome",font=("Arial Black",78,"bold"),bg="red")
 label.pack(pady=100)
 window.mainloop()
