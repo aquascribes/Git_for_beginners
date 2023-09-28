47106f796c911171d373f74dcc3002013207c260
aquascribes <aqsamushtaq@gmail.com>
2023-09-28 12:16:22 +0200

    removed welcome message

--- digiproj.py
+++ digiproj.py
@@ -5,6 +5,6 @@ window.title("digital clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label=Label(window,text="welcome",font=("Arial Black",78,"bold"),bg="steelblue")
+label=Label(window,font=("Arial Black",78,"bold"),bg="steelblue")
 label.pack(pady=100)
 window.mainloop()
